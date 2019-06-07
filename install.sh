8627743d544b89d08f7b21f8c862260d6f5dec94
drakemain <demain1990@gmail.com>
2019-06-07 15:56:06 -0700

    vim plugins

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@
 
 cp ./.vimrc_home ~/.vimrc
 cp ./.gvimrc_home ~/.gvimrc
+
+mkdir -p ~/.vim/bundle
+
+cd ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ./Vundle.vim
+vim +PluginInstall +qall
+
+cd YouCompleteMe
+python3 install.py --rust-completer --clang-completer
